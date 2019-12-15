07d22e6c98df858dbc63c22a0ac1b5868bf27a82
Will Turbaevsky <turbaevsky@gmail.com>
2019-12-15 20:57:50 +0000

    added reporting and monitoring modules

--- index.py
+++ index.py
@@ -7,14 +7,24 @@ from dash.dependencies import Input, Output, State
 
 from app import app
 from apps import business, sentiment, cv
+from apps import monitor
 import layouts
+
+from pages import (
+    overview,
+    pricePerformance,
+    portfolioManagement,
+    feesMins,
+    distributions,
+    newsReviews,
+)
 
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
-],
-style={'background-image': 'linear-gradient(to right, lightblue, lightgreen)'})
+],)
+#style={'background-image': 'linear-gradient(to right, lightblue, lightgreen)'})
 
 
 @app.callback(Output('page-content', 'children'),
@@ -26,6 +36,29 @@ def display_page(pathname):
         return layouts.navbar, sentiment.body
     elif pathname == '/apps/cv':
         return layouts.navbar, cv.body
+    elif pathname == '/dash-financial-report/overview':
+        return overview.create_layout(app)
+    elif pathname == "/dash-financial-report/price-performance":
+        return pricePerformance.create_layout(app)
+    elif pathname == "/dash-financial-report/portfolio-management":
+        return portfolioManagement.create_layout(app)
+    elif pathname == "/dash-financial-report/fees":
+        return feesMins.create_layout(app)
+    elif pathname == "/dash-financial-report/distributions":
+        return distributions.create_layout(app)
+    elif pathname == "/dash-financial-report/news-and-reviews":
+        return newsReviews.create_layout(app)
+    elif pathname == "/dash-financial-report/full-view":
+        return (
+            overview.create_layout(app),
+            pricePerformance.create_layout(app),
+            portfolioManagement.create_layout(app),
+            feesMins.create_layout(app),
+            distributions.create_layout(app),
+            newsReviews.create_layout(app),
+        )
+    elif pathname == '/apps/monitor':
+        return monitor.body
     else:
         return layouts.navbar, layouts.topic, layouts.cards
 
--- layouts.py
+++ layouts.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 
-topic = html.H5('''
+topic = html.H6('''
     All the code, including Machine Learning algorithms, which is used at the site is real and \
     can be easily implemented into your application or Soft-as-a-Service.
     ''', style = {'background-image': 'linear-gradient(to right, lightgrey , lightgrey)'})
@@ -13,7 +13,7 @@ card_content_1 = [
     dbc.CardHeader("Card header"),
     dbc.CardBody(
         [
-            html.H5("Card title", className="card-title"),
+            html.H6("Card title", className="card-title"),
             html.P(
                 "This is some card content that we'll reuse",
                 className="card-text",
@@ -47,14 +47,14 @@ card_content_dataScience = [
     dbc.CardImg(src="/assets/images/back2.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Data Science to help your business", className="card-title"),
+            html.H6("Data Science to help your business", className="card-title"),
             html.P(
                 "Human should create but computer must work - this is the basic principle for our business. \
                 Suppose you have a little business and need to predict your sales. Click the button below and \
                 try to feel your sales during the time then predict your profit using different models",
                 className="card-text",
             ),
-            dbc.Button("Make prediction", color="primary", href="/apps/business"),
+            dbc.Button("Make prediction", color="success", href="/apps/business"),
         ]
     ),
 ]
@@ -64,7 +64,7 @@ card_content_OCR = [
     dbc.CardImg(src="/assets/images/ocr.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Optical character recognition", className="card-title"),
+            html.H6("Optical character recognition", className="card-title"),
             html.P(
                 "Optical character recognition or optical character reader (OCR) \
                 is the electronic or mechanical conversion of images of typed, \
@@ -86,7 +86,7 @@ card_content_time = [
     dbc.CardImg(src="/assets/images/time.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Time-series prediction and clasterisation", className="card-title"),
+            html.H6("Time-series prediction and clasterisation", className="card-title"),
             html.P(
                 "Out system also available to predict time-series, for example, energy or gas \
                 consumption to plan your business. Additionaly, we are able to identify the \
@@ -103,7 +103,7 @@ card_content_vision = [
     dbc.CardImg(src="/assets/images/vision.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Computer Vision and Human Recognition", className="card-title"),
+            html.H6("Computer Vision and Human Recognition", className="card-title"),
             html.P(
                 "We have devceloped a simple sysrtem to recognize humans at the CCTV. It will let \
                 you save your time and make your CCTV system more effective. Try to upload your image \
@@ -111,7 +111,7 @@ card_content_vision = [
                 even for microcomputers like Raspberry PI",
                 className="card-text",
             ),
-            dbc.Button("Try it", color="primary", href='/apps/cv'),
+            dbc.Button("Try it", color="success", href='/apps/cv'),
         ]
     ),
 ]
@@ -121,13 +121,13 @@ card_content_monitor = [
     dbc.CardImg(src="/assets/images/monitor2.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Online process monitoring", className="card-title"),
+            html.H6("Online process monitoring", className="card-title"),
             html.P(
                 "Our system lets you to monitor yons of the parameters creating user-friendly \
                 dashboard. It can include various parameters including numbers, images, levels etc.",
                 className="card-text",
             ),
-            dbc.Button("Example", color="primary", disabled=True),
+            dbc.Button("Example", color="success", href='/apps/monitor'),
         ]
     ),
 ]
@@ -137,14 +137,14 @@ card_content_online = [
     dbc.CardImg(src="/assets/images/online.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Natural Language Processing", className="card-title"),
+            html.H6("Natural Language Processing", className="card-title"),
             html.P(
                 "It is very similar to online process monitoring but more focused at news or tweet \
                 monitoring to recognize trends and providing sentiment analysis. It looks useful for \
                 risk assessment, insuranse related business and online trading",
                 className="card-text",
             ),
-            dbc.Button("Try", color="primary", href='/apps/sentiment'),
+            dbc.Button("Try", color="success", href='/apps/sentiment'),
         ]
     ),
 ]
@@ -154,7 +154,7 @@ card_content_scrap = [
     dbc.CardImg(src="/assets/images/scrap.jpeg", top=True),
     dbc.CardBody(
         [
-            html.H5("Web scapping and processing", className="card-title"),
+            html.H6("Web scapping and processing", className="card-title"),
             html.P(
                 "There are lot of data available in the Internet however very few of them are available \
                 in machine-readable format. Our project will help you to scrap all the data around the \
@@ -172,7 +172,7 @@ card_content_reports = [
     dbc.CardImg(src="/assets/images/report.png", top=True),
     dbc.CardBody(
         [
-            html.H5("Report creation", className="card-title"),
+            html.H6("Report creation", className="card-title"),
             html.P(
                 "It is quite similar to online monitoring and processing but more focused at the \
                 data processing to create printable reports. If you have lots of data which does not \
@@ -181,7 +181,7 @@ card_content_reports = [
                 Of course, it will be used with the very modern Artificial Intellegence technology.",
                 className="card-text",
             ),
-            dbc.Button("Report example", color="primary", disabled=True),
+            dbc.Button("Report example", color="success", href='/dash-financial-report/overview'),
         ]
     ),
 ]
@@ -223,19 +223,19 @@ search_bar = dbc.Row(
 )
 
 navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Main page", href="/")),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("More pages", header=True),
-                dbc.DropdownMenuItem("Page 2", href="#"),
-                dbc.DropdownMenuItem("Page 3", href="#"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
-    ],
+    #children=[
+    #    dbc.NavItem(dbc.NavLink("Main page", href="/")),
+    #    dbc.DropdownMenu(
+    #        children=[
+    #            dbc.DropdownMenuItem("More pages", header=True),
+    #            dbc.DropdownMenuItem("Page 2", href="#"),
+    #            dbc.DropdownMenuItem("Page 3", href="#"),
+    #        ],
+    #        nav=True,
+    #        in_navbar=True,
+    #        label="More",
+    #    ),
+    #],
     brand="Data Science Delivery Company",
     brand_href="/",
     color="primary",
