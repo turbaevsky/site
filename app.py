7874f3238a1b2f7eda51040c70977ed6841d1da7
Will Turbaevsky <turbaevsky@gmail.com>
2019-12-27 16:01:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)#, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__)  # ,routes_pathname_prefix='/demo/' )
+#, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 app.config.suppress_callback_exceptions = True
